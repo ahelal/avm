b4b974f76db93b29e36a2debcbb77aafbc8e674b
Adham Helal <aa@hellofresh.com>
2015-12-19 00:19:12 +0100

    test Installation setup re-structure

--- tests/installation.sh
+++ tests/installation.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bats
 
+#Switch to v2
+ansible-version set v2
 
-@test "Installation Check boto is installed by installer on v2" {
-    command ansible-version set v2
+@test "Installation Check boto is installed by installer on v2" {    
     run python -c "import boto"
     [ $status = 0 ]
 }
