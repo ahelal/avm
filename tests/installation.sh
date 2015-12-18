5afe63cd3f677b75d2723c168662f30d9fcbbb06
Adham Helal <aa@hellofresh.com>
2015-12-19 00:49:51 +0100

    test basic installation type and requirement file

--- tests/installation.sh
+++ tests/installation.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bats
 
-#Switch to v2
-ansible-version set v2
+@test "Installation check v1 virtualenv has no boto" {    
+    run $HOME/.venv_ansible/v1/venv/bin/python -c "import boto"
+    [ $status = 1 ]
+}
 
-@test "Installation Check boto is installed by installer on v2" {    
-    run python -c "import boto"
+@test "Installation Check v1 has no git dir" {    
+    run stat $HOME/.venv_ansible/v1/ansible/.git
+    [ $status = 1 ]
+}
+
+@test "Installation check v2 virtualenv has no boto" {      
+    run $HOME/.venv_ansible/v2/venv/bin/python -c "import boto" 
+    [ $status = 0 ]
+}
+
+@test "Installation Check v2 has git dir" {    
+    run stat $HOME/.venv_ansible/v2/ansible/.git
     [ $status = 0 ]
 }
 
