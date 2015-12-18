5b5fa201e5014ef1473b3a4ce13ef4d2bac48b3f
Adham Helal <aa@hellofresh.com>
2015-12-18 23:00:50 +0100

    Add tests

---
+++ tests/various_runs.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bats
+
+@test "By default we should be running ansible v1 1.9.x" {
+    run ansible --version
+    [[ ${lines[0]} =~ "ansible 1.9" ]]
+}
+
+@test "Run ansible ad-hoc ping pong" {
+    run ansible -i localhost, -c local -m ping all
+    [ "$status" = 0 ]
+    [[ ${lines[0]} =~ "localhost | success" ]]
+}
+
+@test "Run ansible copy" {
+    run ansible -i localhost, -c local -m ping -m copy -a "src=various_runs.sh dest=/tmp/various_runs.sh mode=0666" all
+    [ "$status" = 0 ]
+    [[ ${lines[0]} =~ "localhost | success" ]]
+}
+
+@test "Run stat on /tmp/various_runs.sh" {
+    run stat /tmp/various_runs.sh
+    [ $status = 0 ]
+}
+
+@test "Run Check boto is installed by installer" {
+    run python -c "import boto"
+    [ $status = 0 ]
+}
+
