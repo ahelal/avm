3dd0abcee14adb9f5212601a53637013193d6fb1
Adham Helal <aa@hellofresh.com>
2016-01-04 20:40:31 +0100

    Add common setup user

---
+++ tests/common_setup_user.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bats
+
+
+@test "Check that ansible is installed" {
+    command -v ansible
+}
+
+@test "Check that ansible-playbook is installed" {
+    command -v ansible-playbook
+}
+
+@test "Check that ansible-playbook is installed" {
+    command -v ansible-playbook
+}
+
+@test "Check that ansible-doc is installed" {
+    command -v ansible-doc
+}
+
+@test "Check that ansible-galaxy is galaxy" {
+    command -v ansible-galaxy
+}
+
+@test "Check that ansible-playbook is installed" {
+    command -v ansible-playbook
+}
+
+@test "Check that ansible-pull is installed" {
+    command -v ansible-pull
+}
+
+@test "Check that ansible-vault is installed" {
+    command -v ansible-vault
+}
