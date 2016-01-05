c145937462e73f67d182d16768f58db53e550576
Adham Helal <aa@hellofresh.com>
2016-01-05 12:57:53 +0100

    reanmed filename in various_run test

--- tests/various_runs_test.sh
+++ tests/various_runs_test.sh
@@ -11,11 +11,11 @@
 }
 
 @test "Run ansible copy" {
-    run ansible -i localhost, -c local -m ping -m copy -a "src=various_runs.sh dest=/tmp/various_runs.sh mode=0666" all
+    run ansible -i localhost, -c local -m ping -m copy -a "src=various_runs_test.sh dest=/tmp/various_runs.sh mode=0666" all
     [[ ${lines[0]} =~ "localhost | success" ]]
 }
 
 @test "Run stat on /tmp/various_runs.sh" {
-    run stat /tmp/various_runs.sh
+    run stat /tmp/various_runs_test.sh
     [ $status = 0 ]
 }
