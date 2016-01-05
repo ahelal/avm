7ff17a580ceedb3546acf3448680d26c4ff9f7f5
Adham Helal <aa@hellofresh.com>
2016-01-05 12:14:48 +0100

    Add setup_user setup

---
+++ tests/installation_setup_user.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bats
+
+@test "Installation check v1 virtualenv has no boto" {    
+    run /home/travis-setup/.venv_ansible/v1/venv/bin/python -c "import boto"
+    [ $status = 1 ]
+}
+
+@test "Installation Check v1 has no git dir" {    
+    run stat /home/travis-setup/.venv_ansible/v1/ansible/.git
+    [ $status = 1 ]
+}
+
+@test "Installation check v2 virtualenv has no boto" {      
+    run /home/travis-setup/.venv_ansible/v2/venv/bin/python -c "import boto" 
+    [ $status = 0 ]
+}
+
+@test "Installation Check v2 has git dir" {    
+    run stat /home/travis-setup/.venv_ansible/v2/ansible/.git
+    [ $status = 0 ]
+}
+
+@test "Check venv perm" {    
+    run stat /home/travis-setup/.venv_ansible/
+    [ $status = 0 ]
+    [[ ${lines[0]} =~ "travis-setup" ]]
+}
