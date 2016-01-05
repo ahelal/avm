53ed638fd2ec73ebc505743d9e26b912dd3434c5
Adham Helal <aa@hellofresh.com>
2016-01-05 12:33:45 +0100

    remove unused test

--- tests/installation_setup_user_test.sh
+++ tests/installation_setup_user_test.sh
@@ -10,17 +10,12 @@
     [ $status = 1 ]
 }
 
-@test "Installation check v2 virtualenv has no boto" {      
-    run /home/travis-setup/.venv_ansible/v2/venv/bin/python -c "import boto" 
-    [ $status = 0 ]
-}
-
 @test "Installation Check v2 has git dir" {    
     run stat /home/travis-setup/.venv_ansible/v2/ansible/.git
     [ $status = 0 ]
 }
 
-@test "Check venv perm" {    
+@test "Check venv_ansible owner is travis-setup" {    
     run stat /home/travis-setup/.venv_ansible/
     [ $status = 0 ]
     [[ ${lines[0]} =~ "travis-setup" ]]
