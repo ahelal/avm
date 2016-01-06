c3c282e7e7f9184ca7ce63010d23827a11156c12
Adham Helal <aa@hellofresh.com>
2016-01-06 23:22:43 +0100

    Also do a run without being verbose

--- example/travis-setup-user.sh
+++ example/travis-setup-user.sh
@@ -7,9 +7,6 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # Use travis commit
 SETUP_VERSION=${TRAVIS_COMMIT_RANGE##*...}
 
-# Be more verbose
-SETUP_VERBOSITY=""
-
 ## Install Ansible 1.9.4
 ANSIBLE_VERSIONS[0]="1.9.4"
 INSTALL_TYPE[0]="pip"
