4ba4ea53980bcac6f239258b142fac701008cd38
Adham Helal <aa@hellofresh.com>
2016-07-12 10:36:12 +0200

    make default v2

--- example/travis-setup-basic.sh
+++ example/travis-setup-basic.sh
@@ -28,14 +28,14 @@ ANSIBLE_VERSIONS[0]="1.9.4"
 INSTALL_TYPE[0]="pip"
 ANSIBLE_V1_PATH="${ANSIBLE_VERSIONS[0]}"    # v1
 
-## Install Ansible stable-2.0 
+## Install Ansible stable-2.0
 ANSIBLE_VERSIONS[1]="stable-2.0"
 PYTHON_REQUIREMENTS[1]="$DIR/python_requirements.txt"
 INSTALL_TYPE[1]="git"
 ANSIBLE_V2_PATH="${ANSIBLE_VERSIONS[1]}"  # v2
 
 # Whats the default version
-ANSIBLE_DEFAULT_VERSION="ANSIBLE_VERSIONS[0]"
+ANSIBLE_DEFAULT_VERSION="ANSIBLE_VERSIONS[1]"
 
 ## Create a temp dir
 filename=$( echo ${0} | sed  's|/||g' )
