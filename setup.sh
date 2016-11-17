440024e0a04cc04572d5c26e255d56a8b1dbbe86
Adham Helal <aa@hellofresh.com>
2016-11-17 11:56:55 +0100

    Fix setup for ubuntu

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ SETUP_USER="${SETUP_USER-$USER}"
 SETUP_USER_HOME="${SETUP_USER_HOME:-$(eval echo ~${SETUP_USER})}"
 
 ## Ubuntu apt pre-req
-UBUNTU_PKGS="${UBUNTU_PKGS:-python-setuptools python-dev build-essential libffi-dev libssl-dev curl software-properties-common}"
+UBUNTU_PKGS="${UBUNTU_PKGS:-python-setuptools python-dev build-essential libffi-dev libyaml-dev libssl-dev curl software-properties-common}"
 
 ## Ansible virtual environment directory
 ANSIBLE_BASEDIR="${ANSIBLE_BASEDIR:-$SETUP_USER_HOME/.venv_ansible}"
@@ -192,7 +192,7 @@ setup_ubuntu(){
     VER=$(lsb_release -sr)
     echo "| Updating some ubuntu-${VER} packages (might take some time)"
     if [ "${VER}" == "14.04" ]; then
-      RUN_COMMAND_AS "sudo apt-get install -y $UBUNTU_PKGS"
+      RUN_COMMAND_AS "sudo apt-get install -y ${UBUNTU_PKGS}"
     elif [ "${VER}" == "16.04" ]; then
       RUN_COMMAND_AS "sudo apt -y update"
       RUN_COMMAND_AS "sudo apt install -y python-minimal"
